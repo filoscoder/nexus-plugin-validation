2065eb75fa282966bb2042b32742af64935d12de
filoscoder <sondaniel.88@gmail.com>
2020-09-21 12:02:20 +0900

    Init configs

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    env: {
+        es2020: true,
+        node: true,
+    },
+    extends: [
+        'plugin:jest/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+        'plugin:import/errors',
+    ],
+    plugins: ['simple-import-sort'],
+    ignorePatterns: ['**/node_modules/', '**/examples/'],
+    settings: {
+        'import/resolver': {
+            typescript: {
+                directory: './tsconfig.json',
+            },
+        },
+    },
+    rules: {
+        'simple-import-sort/sort': 'error',
+        '@typescript-eslint/ban-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+};
